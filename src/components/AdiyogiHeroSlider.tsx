445a99f251645a46ec5b6a3fc275fe9fd40b6c45
Mannat Travels Admin <AadiPaul68@gmail.com>
2026-09-26 05:21:40 +0000

    Update hero auto slider interval to 2 seconds

--- src/components/AdiyogiHeroSlider.tsx
+++ src/components/AdiyogiHeroSlider.tsx
@@ -109,13 +109,13 @@ export const AdiyogiHeroSlider: React.FC<AdiyogiHeroSliderProps> = ({ onOpenQuot
     if (autoNextRef.current) clearTimeout(autoNextRef.current);
     autoNextRef.current = setTimeout(() => {
       handleNext();
-    }, 3000);
+    }, 2000);
   };
 
   useEffect(() => {
     autoNextRef.current = setTimeout(() => {
       handleNext();
-    }, 3000);
+    }, 2000);
 
     return () => {
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
